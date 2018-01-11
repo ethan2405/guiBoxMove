6a3ff9b857eae9c6b9b897a0d6d289244e8d95e7
Ethan Moore <32571850+ethan2405@users.noreply.github.com>
2018-01-11 11:59:42 +0000

    Add files via upload

---
+++ guiBoxMove/guiBoxMove/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace guiBoxMove
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.BackColor = System.Drawing.Color.LightGray;
+            this.btnUp.Location = new System.Drawing.Point(463, 362);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(60, 60);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "/\\";
+            this.btnUp.UseVisualStyleBackColor = false;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.BackColor = System.Drawing.Color.LightGray;
+            this.btnRight.Location = new System.Drawing.Point(529, 424);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(60, 60);
+            this.btnRight.TabIndex = 2;
+            this.btnRight.Text = ">";
+            this.btnRight.UseVisualStyleBackColor = false;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.BackColor = System.Drawing.Color.LightGray;
+            this.btnLeft.Location = new System.Drawing.Point(397, 424);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(60, 60);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "<";
+            this.btnLeft.UseVisualStyleBackColor = false;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.BackColor = System.Drawing.Color.LightGray;
+            this.btnDown.Location = new System.Drawing.Point(463, 490);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(60, 60);
+            this.btnDown.TabIndex = 4;
+            this.btnDown.Text = "\\/";
+            this.btnDown.UseVisualStyleBackColor = false;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click_1);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.Location = new System.Drawing.Point(36, 25);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(63, 70);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(607, 577);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnUp);
+            this.Name = "frmMain";
+            this.Text = "Move the Box";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ guiBoxMove/guiBoxMove/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace guiBoxMove
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeft_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Left = this.pictureBox1.Left -= 10;
+        }
+
+        private void btnUp_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Top = this.pictureBox1.Top -= 10;
+        }
+
+        private void btnRight_Click(object sender, EventArgs e)
+        {
+            this.pictureBox1.Left = this.pictureBox1.Left += 10;
+        }
+
+        private void btnDown_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnDown_Click_1(object sender, EventArgs e)
+        {
+            this.pictureBox1.Top = this.pictureBox1.Top += 10;
+        }
+    }
+}
